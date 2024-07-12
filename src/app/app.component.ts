4537a3800cb71fe7d42f12825f05c4add4cf5ec4
Chaos-19 <kalgetachew375@gmail.com>
2024-07-13 00:47:56 +0300

    first angular tutorial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core"
+import { RouterModule, RouterLink, RouterOutlet } from "@angular/router"
+import { HomeComponent } from "./home/home.component"
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: "app-root",
+    standalone: true,
+    imports: [HomeComponent, RouterModule, RouterLink, RouterOutlet],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.css"
 })
 export class AppComponent {
-  title = 'GettingStarted';
+    title = "first-angular-app"
 }
